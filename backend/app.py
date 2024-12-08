57c7a6f7fc0d8ffd85a40fc163e441ad66b7d996
McMuFln <74050977+mufln@users.noreply.github.com>
2024-12-08 22:33:19 +0300

    docker-compose

--- backend/app.py
+++ backend/app.py
@@ -8,13 +8,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from marker.converters.pdf import PdfConverter
 from marker.models import create_model_dict
 from marker.output import text_from_rendered
+import ollama
 import re
 
 converter = PdfConverter(
     artifact_dict=create_model_dict(),
 )
-
-
 
 app = FastAPI()
 app.add_middleware(
@@ -25,18 +24,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class Formula(BaseModel):
     name: str
     latex: str
     source: str
 
+
 class FormulaInDb(Formula):
     id: int
 
 
-
 def connection_factory():
-    return psycopg.connect("postgresql://wysiwyg:randomPassword123!@localhost/wysiwyg",)
+    return psycopg.connect("postgresql://wysiwyg:randomPassword123!@localhost/wysiwyg", )
 
 
 @app.get("/formulas")
@@ -46,11 +46,14 @@ def get_formulas(db: Annotated[psycopg.Connection, Depends(connection_factory)])
     all = cur.fetchall()
     return list(map(FormulaInDb.model_validate, all))
 
+
 @app.post("/formulas")
 def create_formula(db: Annotated[psycopg.Connection, Depends(connection_factory)], formula: Formula):
     cur = db.cursor(row_factory=dict_row)
-    cur.execute("INSERT INTO formulas(name, latex, source) VALUES (%s, %s, %s)", (formula.name, formula.latex, formula.source))
+    cur.execute("INSERT INTO formulas(name, latex, source) VALUES (%s, %s, %s)",
+                (formula.name, formula.latex, formula.source))
     db.commit()
+
 
 @app.post("/parse")
 def parse_pdf(db: Annotated[psycopg.Connection, Depends(connection_factory)], file: UploadFile):
@@ -65,4 +68,5 @@ def parse_pdf(db: Annotated[psycopg.Connection, Depends(connection_factory)], fi
     for match in matches_large:
         cur.execute("INSERT INTO formulas(name, latex, source) VALUES (%s, %s, %s)", ("", match.strip('$'), "users"))
     db.commit()
-    return list(map(lambda x: x.strip('$'), matches_large)), list(map(lambda x: x.strip('$'), filter(lambda x: x != '$$', matches_small)))
+    return list(map(lambda x: x.strip('$'), matches_large)), list(
+        map(lambda x: x.strip('$'), filter(lambda x: x != '$$', matches_small)))
